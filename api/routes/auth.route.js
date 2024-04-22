524e44a54e3fbcb6b3281532d62d58a61baa1659
MutazaMadadi <murtazamadadi12@gmail.com>
2024-04-22 10:09:15 +0330

    sign in api route and controller function and create token

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const route=express.Router();
 
 
 route.post("/sign-up",signup)
+route.post("/sign-in",signin)
 
 export default route;
