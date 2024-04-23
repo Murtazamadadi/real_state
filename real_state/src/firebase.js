00dc4ffff66f121a3bed4ca4425ed0037bd90701
MutazaMadadi <murtazamadadi12@gmail.com>
2024-04-23 07:47:49 +0330

    completed google authentication for sign in

---
+++ real_state/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD01ngSS5-oAo3Nou_8qxo23RM0BNOzAj4",
+  authDomain: "home-86d95.firebaseapp.com",
+  projectId: "home-86d95",
+  storageBucket: "home-86d95.appspot.com",
+  messagingSenderId: "598075363811",
+  appId: "1:598075363811:web:bdd5a8ca19de7fe10390d6"
+
+};
+
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
--- real_state/src/pages/SignUp.jsx
+++ real_state/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -70,7 +71,7 @@ export default function SignUp() {
         >
           {loading ? 'پردازش ...' : 'ثبت نام'}
         </button>
-       {/* <OAuth/> */}
+       <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>آيا حساب کاربری دارید؟</p>
