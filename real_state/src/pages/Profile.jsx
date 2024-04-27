d90b2b35bb72ba95a026147a09ab97a3dc59e4c6
MutazaMadadi <murtazamadadi12@gmail.com>
2024-04-27 10:05:21 +0330

    complete user create listing functionality

--- real_state/src/pages/Profile.jsx
+++ real_state/src/pages/Profile.jsx
@@ -28,6 +28,9 @@ export default function Profile() {
   //========================================================= after user update
   // console.log(formData)
   const [updateSuccess,setUpdateSuccess]=useState(false)
+  //========================================================= after show listing for user 
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   // ====================== profile image uploading step -3
   // firebase storage
@@ -128,6 +131,23 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  // ============================================================ handleShowListing 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -216,7 +236,55 @@ export default function Profile() {
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
-      </p>      
+      </p>
+
+        {/* ===================================================== user show listings functionality */}
+       <button onClick={handleShowListings} className='text-green-700 w-full'>
+        دیدن لیست
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+
+       {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            لیست شما
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  // onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  خذف
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>بروزرسانی</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}      
     </div>
   );
 }
