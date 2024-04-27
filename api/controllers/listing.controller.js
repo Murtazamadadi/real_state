928b96f221e0b68a30a2664e44725101af445603
MutazaMadadi <murtazamadadi12@gmail.com>
2024-04-27 11:39:07 +0330

    update route ape for user listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/customError.js"
 
 export const createListing= async (req,res,next)=>{
     try{
@@ -26,5 +27,29 @@ export const deleteListing= async(req,res,next)=>{
       res.status(200).json('کاربر موفقانه حذف شد');
     } catch (error) {
       next(error);
+    }
+}
+
+
+export const updateListing=async(req,res,next)=>{
+    const listing= await Listing.findById(req.params.id)
+
+    if(!listing){
+        return next(errorHandler(404,"لیست پیدا نشد"))
+    }
+
+    if(req.user.id!== req.params.id){
+        return next(errorHandler(401,"شما فقد میتوانید اکوانت خویش را بروزرسانی کنید"))
+    }
+
+    try{
+        const updateListing=await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+        res.status(200).json(updateListing)
+    }catch(error){
+        next(error)
     }
 }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing,updateListing } from "../controllers/listing.controller.js";
 
 const route=express.Router();
 
 route.post("/create",verifyToken,createListing)
 route.delete("/delete/:id",verifyToken,deleteListing)
+route.post("/update/:id",verifyToken,updateListing)
 
 export default route;
